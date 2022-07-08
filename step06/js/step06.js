340eef906e9b5b2241cc2c81cc47b96bbef18ef2
bbayong28 <ghkdud5795@hanmail.net>
2022-07-08 12:02:42 +0900

    220708

---
+++ step06/js/step06.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $('.sub_menu').slideUp()
+        $(this).next().stop().slideToggle();
+
+    })
+
+
+    $('.mbtn').on('click', function () {
+        $('.gnb').toggleClass('on');
+    })
+
+
+})   
